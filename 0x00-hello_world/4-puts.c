73e0eec0e631b9aff1ba34e837d2f6b374cc8226
Mmamdi-Tech <faithfulnessnwabunor@gmail.com>
2022-09-12 02:41:54 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,24 +1,13 @@
 #!/bin/bash
 #include <stdio.h>
 
-
-
 /**
+ * main - Printing out
  *
- *  * main - Printing out
- *
- *   *
- *
- *    * Return: Always 0 (Successful)
- *
- *     */
-
+ * Return: Always 0 (Successful)
+ */
 int main(void)
-
 {
-
-		puts("\"Programming is like building a multilingual puzzle");
-
-			return (0);
-
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }  
