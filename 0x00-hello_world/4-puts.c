55930718da011d3ff3119249ec5658216b9ce393
Mmamdi-Tech <faithfulnessnwabunor@gmail.com>
2022-09-12 02:33:50 -0700

    Hello,puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,20 +5,20 @@
 
 /**
  *
- * * main - Printing
+ *  * main - Printing out
  *
- * *
+ *   *
  *
- * * Return: Always 0 (Successful)
+ *    * Return: Always 0 (Successful)
  *
- * */
+ *     */
 
 int main(void)
 
 {
 
-	puts("\"Programming is like building a multilingual puzzle");
+		puts("\"Programming is like building a multilingual puzzle");
 
-	return (0);
+			return (0);
 
-}
+}  
