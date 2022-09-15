cb0a7aeab29094f4468f5871ed370c75dee07b51
Mmamdi-Tech <faithfulnessnwabunor@gmail.com>
2022-09-14 17:49:24 -0700

    10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10
+ *
+ * Return: Always 0 (Is a Success)
+ */
+void print_alphabet_x10(void)
+{
+	int b = 0;
+	while (b < 10)
+	{
+		char a = 'a';
+		while (a <= 'z')
+		{
+			_putchar(a);
+			a++;
+		}
+		_putchar('\n');
+		b++;
+	}
+}
