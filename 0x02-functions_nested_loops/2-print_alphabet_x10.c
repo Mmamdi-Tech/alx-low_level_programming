7c94f72f994b494002d85d1c4ad35eed415a8a03
Mmamdi-Tech <faithfulnessnwabunor@gmail.com>
2022-09-14 22:14:00 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,16 +8,14 @@
 
 void print_alphabet_x10(void)
 {
-	int b = 0;
-	while (b < 10)
+	char c, i;
+
+	for (i = 0; i <= 9; i++)
 	{
-		char a = 'a';
-		while (a <= 'z')
+		for (c ='a'; c <= 'z'; c++)
 		{
-			_putchar(a);
-			a++;
+			_putchar(c);
 		}
 		_putchar('\n');
-		b++;
 	}
 }
