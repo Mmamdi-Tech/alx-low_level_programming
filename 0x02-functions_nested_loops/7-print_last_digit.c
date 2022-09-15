cf605026fa097d9ab5e166fef18338959243d7e8
Mmamdi-Tech <faithfulnessnwabunor@gmail.com>
2022-09-14 21:57:13 -0700

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -3,17 +3,15 @@
 /**
  * print_last_digit - prints the last digit of a number
  * @n: number being tested
- * Return: Always 0 (Is a Success).
+ * Return: Always 0.
  */
 
 int print_last_digit(int n)
 {
-	int last_digit-n % 10;
+	n = n % 10;
 
-	if (last_digit < n)
-		last_digit *= -1;
-
-	_putchar(last_digit + '0');
-
-	return(last_digit);
+	if (n < 0)
+		n = -n;
+	_putchar(n + '0');
+	return (n);
 }
