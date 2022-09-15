7a168fd7fd1f46a27ce8bf3814ae6aee48b919bf
Mmamdi-Tech <faithfulnessnwabunor@gmail.com>
2022-09-14 22:05:19 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,10 +1,11 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 - func to ptint out abc 10x
+ * print_alphabet_x10 - func that prints 10 times the alphabet, in lowercase,
  *
- * Return: Always 0 (Is a Success).
+ * Return - 0
  */
+
 void print_alphabet_x10(void)
 {
 	int b = 0;
